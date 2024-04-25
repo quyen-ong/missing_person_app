db615719a8fa6e57e79efa7dc30e46c14add86ae
quyen <quyenong1@gmail.com>
2024-04-25 16:30:07 -0400

    added email function and fixed image

--- lib/display_form.dart
+++ lib/display_form.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+import 'package:url_launcher/url_launcher.dart' as launcher;
 
+//global variables
+File _image = File("");
+String imagePath = "";
+bool addedImage = false;
 
 class FormDisplay extends StatefulWidget {
   const
@@ -14,21 +22,6 @@ class FormDisplay extends StatefulWidget {
 }
 
 class _FormDisplay extends State<FormDisplay> {
-  File _image = File("");
-  final picker = ImagePicker();
-
-  //Image Picker function to get image from gallery
-  Future getImageFromGallery() async {
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      }
-    });
-  }
-
-
   // text controllers
   TextEditingController nameController = TextEditingController();
   TextEditingController genderController = TextEditingController();
@@ -41,6 +34,61 @@ class _FormDisplay extends State<FormDisplay> {
   TextEditingController collectiveController = TextEditingController();
   TextEditingController stationController = TextEditingController();
   TextEditingController dateController = TextEditingController();
+
+
+  final picker = ImagePicker();
+
+  //Image Picker function to get image from gallery
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        imagePath = pickedFile.path;
+        _image = File(pickedFile.path);
+      }
+    });
+  }
+
+  //send email
+  Future<void> _sendEmail() async{
+    String name = nameController.text;
+    String gender = genderController.text;
+    String age = ageController.text;
+    String height = heightController.text;
+    String weight = weightController.text;
+    String eye = eyeController.text;
+    String hair = hairController.text;
+    String officer = officerController.text;
+    String collective = collectiveController.text;
+    String station = stationController.text;
+    String date = dateController.text;
+
+
+    String emailBody =
+        'Hi, there is a missing person. Their information is provided below: <br>'
+        '<br>Name: $name<br>'
+        'Gender: $gender<br>'
+        'Age: $age<br>'
+        'Height: $height<br>'
+        'Weight: $weight<br>'
+        'Eye Color: $eye<br>'
+        'Hair Color: $hair<br>'
+        'Officer: $officer<br>'
+        'Collective: $collective<br>'
+        'Station: $station<br>'
+        'Date: $date';
+
+    final Email email = Email(
+      body: emailBody,
+      subject: 'Missing Person',
+      recipients: ['admin@philisaabafazi.org', 'philisa.abafazi.org'],
+      attachmentPaths: [imagePath],
+      isHTML: true,
+    );
+
+    await FlutterEmailSender.send(email);
+  }
 
   @override
   void dispose() {
@@ -71,6 +119,7 @@ class _FormDisplay extends State<FormDisplay> {
                   Navigator.of(context).pop();
                   // get image from gallery
                   getImageFromGallery();
+                  addedImage = true;
                 },
               ),
             ],
@@ -79,43 +128,52 @@ class _FormDisplay extends State<FormDisplay> {
   }
 
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        backgroundColor: Theme.of(context)
-            .colorScheme.inversePrimary,
-        title: const         // see page 3
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: const // see page 3
         Text('Missing Person Form'),
 
-      ),   // AppBar
+      ), // AppBar
 
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView(
           children: <Widget>[
             const Text("The Collective-GBV and Missing Persons Resource",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                ),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
             ),
             const Text("Email: admin@philisaabafazi.org, philisa.abafazi.org"),
             const Text("Phone: 021 565 0668 / 081 746 9889"),
             const Text("Police: 0788 77 21 07 / 071 449 5496"),
             const Text(""),
 
+            //displays the textfields
             _textField(context, nameController, "Name", "Enter Name"),
             _textField(context, genderController, "Gender", "Enter Gender"),
             _textField(context, ageController, "Age", "Enter Age"),
             _textField(context, heightController, "Height", "Enter Height"),
             _textField(context, weightController, "Weight", "Enter Weight"),
             _textField(context, eyeController, "Eye Color", "Enter Eye Color"),
-            _textField(context, hairController, "Hair Color", "Enter Hair Color"),
-            _textField(context, officerController, "Investigating Officer", "Enter Investigating Officer"),
-            _textField(context, collectiveController, "The Collective", "Enter The Collective"),
-            _textField(context, stationController, "Police Station Currently Reported Missing At", "Enter Police Station"),
+            _textField(
+                context, hairController, "Hair Color", "Enter Hair Color"),
+            _textField(context, officerController, "Investigating Officer",
+                "Enter Investigating Officer"),
+            _textField(context, collectiveController, "The Collective",
+                "Enter The Collective"),
+            _textField(context, stationController,
+                "Police Station Currently Reported Missing At",
+                "Enter Police Station"),
             _textField(context, dateController, "Date", "Enter Date"),
 
+            //add image
             const Text('\nAdd Image',
               style: TextStyle(
                 fontSize: 20.0,
@@ -127,9 +185,7 @@ class _FormDisplay extends State<FormDisplay> {
               child: const Text('Select Image'),
             ),
 
-
-            Image.file(_image),
-
+            _displayPic(context),
           ],
         ), // Center
       ), // Padding
@@ -137,35 +193,40 @@ class _FormDisplay extends State<FormDisplay> {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ElevatedButton(
-            child: const Text("Submit Form"),
-            onPressed: () => _onBtnPressed(context)
-        ),  // ElevatedButton
-      ),    // Padding
-    );  // Scaffold
-  }    // build
+          child: const Text("Submit Form"),
+          onPressed: () => _sendEmail(), // ElevatedButton
+        ),
+      ),// Padding
+    ); // Scaffold
+  } // build
 
-  void _onBtnPressed(BuildContext context) {
-    setState(() {
+  //makes the text field
+  Widget _textField(BuildContext context,
+      TextEditingController textFieldController, String label, String hint) {
+    return TextField(
+      controller: textFieldController,
 
-    });
-  }
-}  // end of _SecondPageDisplayState
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+      ), // InputDecoration
 
-Widget _textField( BuildContext context, TextEditingController textFieldController, String label, String hint) {
-  return TextField(
-    controller: textFieldController,
+      onEditingComplete: () {
+        // dismiss keyboard after the
+        // user clicks on the done key
+        FocusScope.of(context)
+            .requestFocus(FocusNode());
+      },
 
-    decoration: InputDecoration(
-      labelText: label,
-      hintText: hint,
-    ), // InputDecoration
+    ); // TextField
+  } // _textField
 
-    onEditingComplete: () {
-      // dismiss keyboard after the
-      // user clicks on the done key
-      FocusScope.of(context)
-          .requestFocus(FocusNode());
-    },
-
-  ); // TextField
-}          // _textField
+  Widget _displayPic(BuildContext context) {
+    if(addedImage){
+      return Image.file(_image);
+    }
+    else{
+      return Image.asset('lib/images/no_photo.png');
+    }
+  } // _textField
+}
